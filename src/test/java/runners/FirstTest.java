2b82ca2b62da9c207662a3c5e74301cdea7f7e22
Juan Pablo Osorio <juanpaosorio199@gmail.com>
2023-10-25 23:23:11 -0500

    Cambios en clase FirstTest

--- src/test/java/runners/FirstTest.java
+++ src/test/java/runners/FirstTest.java
@@ -4,12 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import models.Datum;
+import models.UserData;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import questions.GetAllUsers;
 import questions.ResponseCode;
 import tasks.GetListUsers;
+import tasks.RegisterUser;
+import tasks.UpdateUser;
+
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -46,6 +50,49 @@ public class FirstTest {
         juan.should(
             seeThat("The user email", act -> user.getEmail(), equalTo("george.bluth@reqres.in")));
     
+    }
+
+    @Test
+    public void createUser(){
+
+        Actor juan = Actor.named("juan")
+            .whoCan(CallAnApi.at(restApiUrl));
+
+        UserData newUser = new UserData();
+
+        newUser.setName("morpheusJP2");
+        newUser.setJob("leader");
+
+        juan.attemptsTo(
+                RegisterUser.withInfo(newUser)
+        );
+
+        juan.should(
+            seeThat("The status code", ResponseCode.was(), equalTo(201)));
+
+    }
+
+
+    @Test
+    public void updateClient(){
+
+        Actor juan = Actor.named("juan")
+            .whoCan(CallAnApi.at(restApiUrl));
+
+        int idUser = 672;
+
+        UserData newUserData = new UserData();
+
+        newUserData.setName("morpheusJP3");
+        newUserData.setJob("leader3");
+
+        juan.attemptsTo(
+                UpdateUser.withInfo(idUser, newUserData)
+        );
+
+        juan.should(
+            seeThat("The status code", ResponseCode.was(), equalTo(200)));
+
     }
 
     
